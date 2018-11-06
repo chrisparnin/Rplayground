96dd6f1ae93ec371abe8cecc7fa0df0f325c02f3
Chris Parnin <chris.parnin@gmail.com>
2018-11-06 16:21:56 -0500

    More examples

--- examples.R
+++ examples.R
@@ -1,4 +1,9 @@
 # Will not have NA
 bad_train <- read.csv('train.csv')
 train <- read.csv('train.csv', na.strings = c("NA",""))
-train[is.na(train$Embarked), ]
+#train[is.na(train$Embarked), ]
+
+train[is.na(train$Embarked)==TRUE, ]
+
+mean(train$Fare[train$Pclass==3 & train$Embarked=='S'], na.rm = TRUE)
+
--- examples.py
+++ examples.py
@@ -2,3 +2,5 @@ import pandas as pd;
 
 train = pd.read_csv("train.csv")
 print train[train.Embarked.isnull()]
+
+print train.Fare[(train.Pclass==3) & (train.Embarked=='S')].mean()
