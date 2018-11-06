91b9f4c8fc3bef7610d1ca8041439293f7f717f2
Chris Parnin <chris.parnin@gmail.com>
2018-11-06 16:55:11 -0500

    playing with missing values

--- examples.R
+++ examples.R
@@ -1,9 +1,11 @@
 # Will not have NA
 bad_train <- read.csv('train.csv')
-train <- read.csv('train.csv', na.strings = c("NA",""))
+train <- read.csv('fuzzed.csv', na.strings = c("NA",""))
 #train[is.na(train$Embarked), ]
 
 train[is.na(train$Embarked)==TRUE, ]
 
-mean(train$Fare[train$Pclass==3 & train$Embarked=='S'], na.rm = TRUE)
+train[is.na(train$Fare)==TRUE, ]
 
+mean(train$Fare[train$Pclass==1])
+mean(train$Fare[train$Pclass==1], na.rm = TRUE)
--- examples.py
+++ examples.py
@@ -1,6 +1,10 @@
 import pandas as pd;
 
 train = pd.read_csv("train.csv")
+fuzzed = pd.read_csv("fuzzed.csv")
 print train[train.Embarked.isnull()]
 
 print train.Fare[(train.Pclass==3) & (train.Embarked=='S')].mean()
+
+print train.Fare[(train.Pclass==1)].mean()
+print fuzzed.Fare[(train.Pclass==1)].mean()
